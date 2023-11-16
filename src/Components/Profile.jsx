2ad5a911a9af52f7b3fae56549b060d493e7d03a
Rafay-Dil123 <dilrafay@gmail.com>
2023-11-16 20:45:18 +0500

    Updated Profile page,styling

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styleprofile.css';
+import Navbar from './navbar';
 
 function Profile() {
     const studentData = {
@@ -8,21 +9,33 @@ function Profile() {
         department: 'Computer Science',
         batch: '2023',
         email: 'john.doe@example.com',
+        group:'B+',
+        dob:'23/7/2003',
+        cnic:"3520128929876",
+        nationality:'Pakistani',
+        MobileNo:'03865627389',
+
+     };
+
+     const Academy= {
+        Name: 'Kips',
+        email: 'kips@gmail.com',
      };
   
      return (
-        <div>
+        <div className='pageon'>
            <Navbar />
   
            <div id="page-body">
+            {/* student acaedmic */}
               <section className="home-section">
-                 <div className="text" style={{ fontWeight: 'bolder' }}>
+                 {/* <div className="text" style={{ fontWeight: 'bolder' }}>
                     <i className="bx bx-home"></i>&nbsp;Student Home Page
-                 </div>
+                 </div> */}
   
                  <div className="main-body">
                     <div className="student-info">
-                       <h2>Personal Information</h2>
+                       <h2>Acaedmic Information</h2>
                        <div>
                           <strong>Name:</strong> {studentData.name}
                        </div>
@@ -41,6 +54,62 @@ function Profile() {
                     </div>
                  </div>
               </section>
+
+               {/* academy info */}
+              <section className="home-section">
+                 <div className="main-body">
+                    <div className="student-info">
+                       <h2>Other Academy info</h2>
+                       <div>
+                          <strong>Name:</strong> {Academy.Name}
+                       </div>
+                       
+                       <div>
+                          <strong>Department:</strong> {studentData.department}
+                       </div>
+                       <div>
+                          <strong>Batch:</strong> {studentData.batch}
+                       </div>
+                       <div>
+                          <strong>Email:</strong> {Academy.email}
+                       </div>
+                    </div>
+                 </div>
+              </section>
+
+              {/* personal info */}
+              <section className="home-section">
+                 {/* <div className="text" style={{ fontWeight: 'bolder' }}>
+                    <i className="bx bx-home"></i>&nbsp;Student Home Page
+                 </div> */}
+  
+                 <div className="main-body">
+                    <div className="student-info">
+                       <h2>Personal Information</h2>
+                       <div>
+                          <strong>Name:</strong> {studentData.name}
+                       </div>
+                       <div>
+                          <strong>Blood group:</strong> {studentData.group}
+                       </div>
+                       <div>
+                          <strong>Cnic:</strong> {studentData.cnic}
+                       </div>
+                       <div>
+                          <strong>DOB:</strong> {studentData.dob}
+                       </div>
+                       <div>
+                          <strong>Mobile Number:</strong> {studentData.MobileNo}
+                       </div>
+                       <div>
+                          <strong>Nationality:</strong> {studentData.nationality}
+                       </div>
+                    </div>
+                 </div>
+              </section>
+
+            
+
            </div>
         </div>
      );
--- src/Components/Student.jsx
+++ src/Components/Student.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Attendance from './Attendance'
-
+import Profile from './Profile'
 function Student() {
   return (
     <div>
 
      {/* <Attendance/> */}
-     
+     <Profile/>
     </div>
   )
 }
