8ef828d3bee0f64387942b4fbb74be633ff7bc6c
hamzasheraz <hamzasheraz429@gmail.com>
2023-11-16 21:23:53 +0500

    time table page updated

--- src/Components/TimeTable.jsx
+++ src/Components/TimeTable.jsx
@@ -41,9 +41,12 @@ const TimeTable = () => {
                 </td>
                 <td>
                   <span class="bg-green padding-5px-tb padding-15px-lr border-radius-5 margin-10px-bottom text-white font-size16  xs-font-size13">
-                    Yoga
+                    {lecture_detail.title}
                   </span>
-                  <div class="margin-10px-top font-size14">9:00-10:00</div>
+                  <div class="margin-10px-top font-size14">
+                    {" "}
+                    {lecture_detail.duration}
+                  </div>
                   <div class="font-size13 text-light-gray">Marta Healy</div>
                 </td>
 
