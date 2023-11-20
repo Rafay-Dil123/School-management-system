914b63730b9105b0b4374804f04a16762789a0cc
hamzasheraz <hamzasheraz429@gmail.com>
2023-11-20 18:04:18 +0500

    course register page added

--- src/Components/Student.jsx
+++ src/Components/Student.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Attendance from './Attendance'
-import Profile from './Profile'
-import TimeTable from './TimeTable'
-import Marks from './Marks'
-import FeeGeneration from './FeeGeneration'
+import React from "react";
+import Attendance from "./Attendance";
+import Profile from "./Profile";
+import TimeTable from "./TimeTable";
+import Marks from "./Marks";
+import FeeGeneration from "./FeeGeneration";
+import CourseRegistration from "./CourseRegistration";
 function Student() {
   return (
     <div>
-
-     {/* <Attendance/> */}
-     {/* <TimeTable /> */}
-      {/* <Profile/>  */}
-      {/* <Marks/> */}
-      <FeeGeneration/>
+      {/* <Attendance/> */}
+      {/* <TimeTable /> */}
+      {/* <Profile /> */}
+      <CourseRegistration />
+      {/* <Marks /> */}
+      {/* <FeeGeneration/> */}
     </div>
-  )
+  );
 }
 
-export default Student
+export default Student;
