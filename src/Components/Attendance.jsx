471f87991c02f267e8593a64118f0b01d9a4b5ea
Rafay-Dil123 <dilrafay@gmail.com>
2023-12-01 17:11:46 +0500

    Completed kafi hadh tak

--- src/Components/Attendance.jsx
+++ src/Components/Attendance.jsx
@@ -1,46 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './navbar';
 import '../styleattendance.css';
 
-function Attendance() {
-  const [selectedCourse, setSelectedCourse] = useState('courseA'); // Default course
+function Attendance({rollnumber}) {
+         // Default course
+         let [studentAttendance,setstudentAttendance]=useState([]);
 
-  // Define attendance data for a student in different courses
-  const studentAttendance = {
-    'John Doe': {
-      courseA: [
-        { date: '2023-01-01', status: 'P' },
-        { date: '2023-01-02', status: 'A' },
-        { date: '2023-01-03', status: 'P' },
-        // Add more attendance data for courseA as needed
-      ],
-      courseB: [
-        { date: '2023-01-01', status: 'P' },
-        { date: '2023-01-02', status: 'P' },
-        { date: '2023-01-03', status: 'A' },
-        // Add more attendance data for courseB as needed
-      ],
-      // Add more courses as needed
-    },
-    // Add more students as needed
-  };
+         useEffect(()=>{
+         getinitialdata();
+         },[])
 
-  const handleCourseChange = (event) => {
-    setSelectedCourse(event.target.value);
-  };
+         let getinitialdata=async()=>{
+          console.log(rollnumber)
+          let data=await fetch(`/api/displaystudentattendance/${rollnumber}`);
+          let data2=await data.json();
+          console.log(data2)
+          setstudentAttendance(data2)
+         }
+
+ 
 
   const getAttendanceTable = () => {
-    // For simplicity, using a default student ('John Doe') in this example
-    const studentData = studentAttendance['John Doe'];
-
-    if (!studentData || !studentData[selectedCourse]) {
-      return <p>No attendance data available for the selected course.</p>;
-    }
-
-    // Display attendance table for the default student and selected course
-    const attendanceList = studentData[selectedCourse];
-
     return (
+
       <table>
         <thead>
           <tr>
@@ -49,10 +31,10 @@ function Attendance() {
           </tr>
         </thead>
         <tbody>
-          {attendanceList.map((attendance, index) => (
+          {studentAttendance&&studentAttendance.map((att, index) => (
             <tr key={index}>
-              <td>{attendance.date}</td>
-              <td>{attendance.status}</td>
+              <td>{att.date}</td>
+              <td>{att.attendance}</td>
             </tr>
           ))}
         </tbody>
@@ -65,15 +47,6 @@ function Attendance() {
     
       <div className="attendance-container">
         <h2>Student Attendance</h2>
-
-        <div className="course-selector">
-          <label htmlFor="course">Select course:</label>
-          <select id="course" name="course" value={selectedCourse} onChange={handleCourseChange}>
-            <option value="courseA">course A</option>
-            <option value="courseB">course B</option>
-            {/* Add more courses as needed */}
-          </select>
-        </div>
 
         {/* Display attendance for the default student and selected course */}
         <div className="attendance-table">
